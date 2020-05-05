8ea62baca9d72efea29732e5d99bed70d968ff0b
Jon Holmes <holmjona@isu.edu>
2020-05-04 19:16:21 -0600

    Add project files.

---
+++ PhanonDataReader/wwwroot/js/site.js
@@ -0,0 +1,75 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(function () {
+    $("#btnRead").click(function () {
+        $.ajax({
+            url: "data/students.json"
+            , success: function (data) {
+                var tbl = $("#tblOutput");
+                var studs = data.progress_map;
+                for (var name in studs) {
+                    var tr = $("<tr>");
+                    var tdName = $("<td>").text(name);
+                    tr.append(tdName);
+                    for (var wndx in studs[name]) {
+                        studObj = studs[name][wndx];
+                        var modID = studObj.id;
+                        var modName = getModuleName(modID);
+                        if (modName.indexOf("Project") > -1) {
+                            lessons = studObj.lessons;
+                            for (var lndx in lessons) {
+                                var lesson = lessons[lndx];
+                                var lessName = getLessonName(lesson.id);
+                                var content = $("<span>").text(modName);
+                                var link = $("<a>");
+                                link.text(lessName);
+                                link.attr("href", makeLink(92, lesson.id, lesson.user_id));
+                                link.attr("target", "_new");
+                                var tdID = $("<td>").append(content,link);
+                                tr.append(tdID);
+                            }
+                        }
+                    }
+
+                    tbl.append(tr);
+                }
+            }
+        });
+    });
+});
+
+function getModuleName(id) {
+
+    for (var mndx in modules) {
+        var mod = modules[mndx];
+        if (mod.id == id) {
+            return mod.module.title;
+        }
+    }
+    return "not found";
+}
+
+function getLessonName(id) {
+
+    for (var mndx in modules) {
+        var mod = modules[mndx];
+        for (var lndx in mod.lessons) {
+            var less = mod.lessons[lndx];
+            if (less.id == id) {
+                return less.title;
+            }
+        }
+    }
+    return "not found";
+}
+
+function makeLink(courseId,exerciseId,studentId) {
+    var lnk = "http://phanon.herokuapp.com/#/course/" + courseId
+        + "/user_lesson_review/" + exerciseId
+        + "/user/" + studentId;
+    return lnk;
+    //: S.default.push("/course/" + t.props.courseId + "/submissions/" + e.id + "/student/" + r.user_id
+}
